74d1e681b01f71e0af138fbd10bc8a2d81639879
nyix <rsa17826@email.vccs.edu>
2026-05-08 12:16:04 -0400

    NO MESSAGE SET

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A simple Go autoclicker";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        # The actual package
+        packages.default = pkgs.buildGoModule {
+          pname = "go-autoclicker";
+          version = "1";
+          src = ./.;
+        };
+
+        # Development environment
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+          ];
+        };
+      }
+    );
+}
